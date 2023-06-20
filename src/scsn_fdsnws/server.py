be2d99b733f5fb59c46e808df085fa7d237ab309
Philip Crotwell <crotwell@seis.sc.edu>
2023-06-20 14:50:52 -0400

    more cleanup, try to get static file dir to contain trailing slash but donesnot work yet

--- src/scsn_fdsnws/server.py
+++ src/scsn_fdsnws/server.py
@@ -39,66 +39,6 @@ class Nav(object):
 
         </html>"""
 
-class DataSelect(object):
-    def __init__(self, archive, conf={}):
-        self.archive = archive
-        self.conf = self.configure_defaults(conf)
-        self.max_timerange = datetime.timedelta(hours=int(self.conf["dataselect"]["maxqueryhours"]))
-
-    @cherrypy.expose
-    def index(self):
-        return """<html>
-
-          <head></head>
-
-          <body>
-            <h3>SCSN FDSNWS service</h3>
-
-          </body>
-
-        </html>"""
-
-    @cherrypy.expose
-    def version(self):
-        return f"""<html>
-
-          <head></head>
-
-          <body>
-            <h3>SCSN FDSNWS service</h3>
-            <h3>0.0.1</h3>
-          </body>
-
-        </html>"""
-
-    @cherrypy.expose
-    def echo(self, net, sta, loc, cha, starttime, endtime):
-        cherrypy.log(f"echo {net}.{sta}.{loc}.{cha} {starttime} {endtime}")
-        return f"""<html>
-
-          <head></head>
-
-          <body>
-            <h3>SCSN FDSNWS service</h3>
-            <h3>{net}.{sta}.{loc}.{cha} {starttime} {endtime}</h3>
-          </body>
-
-        </html>"""
-
-
-    def configure_defaults(self, conf):
-        if not "dataselect" in conf:
-            conf["dataselect"] = {}
-        dataselect_conf = conf["dataselect"]
-        if "maxqueryhours" not in dataselect_conf:
-            dataselect_conf["maxqueryhours"] = "24"
-        if "ringserver" in conf:
-            ring_conf = conf["ringserver"]
-            if "port" not in ring_conf:
-                ring_conf["port"] = 80
-            if "host" not in ring_conf:
-                ring_conf["host"] = "127.0.0.1"
-        return conf
 
 
 
@@ -134,16 +74,14 @@ def main():
     cherrypy.config.update({'server.socket_host': "0.0.0.0"})
     conf = {
             '/': {
-                'tools.sessions.on': True,
-                'tools.staticdir.root': os.path.abspath(os.getcwd())
-            },
-            '/static': {
+                'tools.staticdir.root': os.path.abspath(os.getcwd()),
                 'tools.staticdir.on': True,
-                'tools.staticdir.dir': './public'
-            },
-            '/fdsnws': {
-                'tools.staticdir.on': True,
-                'tools.staticdir.dir': './public'
+                'tools.staticdir.dir': './public',
+                'tools.staticdir.index': 'index.html',
+                'tools.trailing_slash.missing': True,
+                #'tools.trailing_slash.on': False,
+                'tools.trailing_slash.status': 301,
+                "tools.staticdir.debug": "True",
             }
         }
     cherrypy.tree.mount(DataSelectWebService(archive, ringconf),
@@ -154,7 +92,6 @@ def main():
                             }
                         }
                         )
-    #cherrypy.tree.mount(DataSelect(archive, ringconf), '/fdsnws/dataselect/1', conf)
     cherrypy.tree.mount(Nav(), '/', conf)
 
     if args.daemon:
